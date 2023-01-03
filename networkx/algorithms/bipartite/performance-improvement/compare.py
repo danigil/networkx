bf0b264a893a3ade78089c2c0d65dd8afcb942da
Daniel <dgilkarov33@gmail.com>
2023-01-03 12:56:20 +0200

    timed functions differently

--- networkx/algorithms/bipartite/performance-improvement/compare.py
+++ networkx/algorithms/bipartite/performance-improvement/compare.py
@@ -1,6 +1,4 @@
-import timeit
-
-import cython.cython_improved
+import cython_improved
 import standard
 import time
 
@@ -47,14 +45,19 @@ def run_standard(func_name, *args, **kwargs):
 envy_free_matching_name = 'envy_free_matching'
 minimum_weight_envy_free_matching_name = 'minimum_weight_envy_free_matching'
 
+def time_func(func, *args, **kwargs):
+    t0 = time.time()
+    func(*args, **kwargs)
+    return time.time() - t0
+
 if __name__ == '__main__':
     sizes = [10, 100, 10000]
     for size in sizes:
         G = generate_marriable_bipartite_graph(size)
         print('--------------------------')
         print(f'running envy_free_matching with G={G}')
-        cython_time = timeit.timeit('run_cython(envy_free_matching_name, G)')
-        standard_time = timeit.timeit('run_standard(envy_free_matching_name, G)')
+        cython_time = time_func(run_cython, envy_free_matching_name, G)
+        standard_time = time_func(run_standard, envy_free_matching_name, G)
         print(f'cython: {cython_time}, standard: {standard_time}, ratio: {standard_time/cython_time}')
         print('--------------------------')
 
