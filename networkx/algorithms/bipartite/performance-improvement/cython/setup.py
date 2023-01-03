bf3a9d8c444a2fc0f9fc598d92c5ce93ae0fc492
Daniel <dgilkarov33@gmail.com>
2023-01-03 20:56:21 +0200

    updated cython setup

--- networkx/algorithms/bipartite/performance-improvement/cython/setup.py
+++ networkx/algorithms/bipartite/performance-improvement/cython/setup.py
@@ -1,7 +1,8 @@
 from setuptools import setup
 from Cython.Build import cythonize
-
+import numpy
 setup(
     name = 'cython_improved',
-    ext_modules = cythonize("cython_improved.pyx")
+    ext_modules = cythonize("cython_improved.pyx"),
+    include_dirs=[numpy.get_include()]
 )
