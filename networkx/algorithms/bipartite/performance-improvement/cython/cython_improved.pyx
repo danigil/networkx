fcfdb1fa8a54f6345acfa317cc61732787ab1263
Daniel <dgilkarov33@gmail.com>
2023-01-03 21:55:52 +0200

    fix21

--- networkx/algorithms/bipartite/performance-improvement/cython/cython_improved.pyx
+++ networkx/algorithms/bipartite/performance-improvement/cython/cython_improved.pyx
@@ -71,10 +71,11 @@ def neighbours_of_set(G, node_set):
         print(f'adjacency_matrix: ', {adjacency_matrix})
         print(f'node_set_as_nparray: ', {node_set_as_nparray})
         try:
-            ret = find_neigbours_in_adjacency_matrix(adjacency_matrix, node_set_as_nparray)
-            print(f'ret: {ret}')
+            find_neigbours_in_adjacency_matrix(adjacency_matrix, node_set_as_nparray)
+            # print(f'ret: {ret}')
 
-            return set(ret)
+            # return set(ret)
+            return set()
         except TypeError:
             print(f'exception')
 
