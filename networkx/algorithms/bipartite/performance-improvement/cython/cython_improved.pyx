4385bc6e0c24934f7faf47082743ba1be0a7c235
Daniel <dgilkarov33@gmail.com>
2023-01-03 22:28:30 +0200

    fix30

--- networkx/algorithms/bipartite/performance-improvement/cython/cython_improved.pyx
+++ networkx/algorithms/bipartite/performance-improvement/cython/cython_improved.pyx
@@ -81,7 +81,7 @@ def neighbours_of_set(G, node_set):
 
 
         try:
-            ret = find_neigbours_in_adjacency_matrix(adjacency_matrix, node_set_as_nparray)
+            ret_value = find_neigbours_in_adjacency_matrix(adjacency_matrix, node_set_as_nparray)
             # print(f'ret: {ret}')
 
             # return set(ret)
