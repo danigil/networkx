2b0727f0c85d0c0663ebffd641dd9435a60826cc
Daniel <dgilkarov33@gmail.com>
2023-05-30 17:04:33 +0300

    Changed input method to manual in-form input

--- networkx/algorithms/bipartite/website/website_code/forms.py
+++ networkx/algorithms/bipartite/website/website_code/forms.py
@@ -5,10 +5,15 @@ from wtforms.validators import DataRequired, Regexp
 
 
 class EnvyFreeMatchingCSVAndTextForm(FlaskForm):
-    file = FileField(label="* Edges CSV File",validators=[
+    file = FileField(label="* Edges CSV File", validators=[
         FileAllowed(['csv'], 'CSVs only!')]
-    )
-    top_nodes = TextAreaField(label='* Top Nodes',validators=[
-        Regexp("^((([0-9])+,)*)(([0-9])+)$",message="Bad Top Nodes input")
+                     )
+    top_nodes = TextAreaField(label='* Top Nodes', validators=[
+        Regexp("^((([0-9])+,)*)(([0-9])+)$", message="Bad Top Nodes input")
     ])
+    submit = SubmitField(label='submit')
+
+
+class EnvyFreeMatchingListAndTextForm(FlaskForm):
+    top_nodes = TextAreaField(label='Top Nodes (Inferred automatically if input is empty)')
     submit = SubmitField(label='submit')
--- networkx/algorithms/bipartite/website/website_code/routes.py
+++ networkx/algorithms/bipartite/website/website_code/routes.py
@@ -2,12 +2,13 @@ import csv
 import io
 import logging
 import os
+import re
 from base64 import b64encode
 
 import networkx as nx
 import networkx.algorithms.bipartite
 from . import app
-from .forms import EnvyFreeMatchingCSVAndTextForm
+from .forms import EnvyFreeMatchingCSVAndTextForm, EnvyFreeMatchingListAndTextForm
 from .algorithms.envy_free_matching import envy_free_matching, minimum_weight_envy_free_matching
 from flask import render_template, redirect, url_for, request, flash, json
 from pandas import read_csv
@@ -32,31 +33,40 @@ def algo_page():
     type = request.args["type"]
     if not type:
         type = "non_weighted"
-    logging.log(level=logging.DEBUG,msg=f"type: {type}")
-    form = EnvyFreeMatchingCSVAndTextForm()
+    logging.log(level=logging.DEBUG, msg=f"type: {type}")
+    form = EnvyFreeMatchingListAndTextForm()
 
     if not form.validate_on_submit():
         logging.log(level=logging.DEBUG, msg=f"rendering algo page")
         return render_template('algo.html', title=f'{type}_algo', form=form, type=type)
     else:
-        input_file = form.file.data
         input_text = form.top_nodes.data
-        logging.log(level=logging.DEBUG, msg=f"got form:\n\tinput_file: {input_file}\n\tinput_text: {input_text}")
+        input_list = request.form.getlist('input_text[]')
+        if input_list and len(input_list) > 0:
 
-        if input_file and input_text:
-            top_nodes = [int(num) for num in input_text.split(',')]
+            if not is_valid_input_list(input_list, type=type):
+                logging.log(level=logging.DEBUG, msg=f'ERROR edges input is malformed')
 
-            edges = read_csv(input_file, header=None)
-            edges = [tup for tup in edges.itertuples(index=False, name=None)]
+                flash(f'ERROR edges input is malformed', category="error")
+                return render_template('algo.html', title='Algo', form=form)
+            if type == "non_weighted":
+                edges = [(int(from_node), int(to_node)) for from_node, to_node in map(lambda s: s.split(','), input_list)]
+            else:
+                edges = [(int(from_node), int(to_node), float(weight)) for from_node, to_node, weight in
+                         map(lambda s: s.split(','), input_list)]
+
+            if input_text == '':
+                top_nodes = [tup[0] for tup in edges]
+            else:
+                top_nodes = [int(num) for num in input_text.split(',')]
 
             logging.log(level=logging.DEBUG, msg=f"edges: {edges}")
             logging.log(level=logging.DEBUG, msg=f"top_nodes: {top_nodes}")
 
-
             if not is_valid_input_csv(type, edges):
-                logging.log(level=logging.DEBUG, msg=f"edge csv file is malformed, EXITING")
+                logging.log(level=logging.DEBUG, msg=f'ERROR edges input is malformed')
 
-                flash(f'ERROR edge csv file is malformed', category="error")
+                flash(f'ERROR edges input is malformed', category="error")
                 return render_template('algo.html', title='Algo', form=form)
 
             logging.log(level=logging.DEBUG, msg=f"calculating matching")
@@ -71,9 +81,10 @@ def algo_page():
             base64img = "data:image/png;base64," + b64encode(file_object.getvalue()).decode('ascii')
 
             logging.log(level=logging.DEBUG, msg=f"redirecting to result page")
-            return render_template('result.html', result=base64img, matching_size=int(len(matching_edges)/2))
+            return render_template('result.html', result=base64img, matching_size=int(len(matching_edges) / 2))
         else:
             flash(f'ERROR missing input', category="error")
+            return render_template('algo.html', title='Algo', form=form)
 
 
 @app.route("/download")
@@ -83,13 +94,25 @@ def download_output_page():
 
 
 valid_input_csv_functions = {
-    "non_weighted": lambda list_of_tup:all(len(tup) == 2 and type(tup[0])==int and type(tup[1])==int for tup in list_of_tup),
-    "weighted": lambda list_of_tup:all(len(tup) == 3 and type(tup[0])==int and type(tup[1])==int and type(tup[2])==float for tup in list_of_tup),
+    "non_weighted": lambda list_of_tup: all(
+        len(tup) == 2 and type(tup[0]) == int and type(tup[1]) == int for tup in list_of_tup),
+    "weighted": lambda list_of_tup: all(
+        len(tup) == 3 and type(tup[0]) == int and type(tup[1]) == int and type(tup[2]) == float for tup in list_of_tup),
 }
 
 
 def is_valid_input_csv(type, edges):
     return valid_input_csv_functions[type](edges)
+
+def is_valid_input_list(list, type = "non_weighted"):
+    for edge_string in list:
+        if type == "non_weighted":
+            if not re.fullmatch('\\d+,\\d+', edge_string):
+                return False
+        else:
+            if not re.fullmatch('\\d+,\\d+,\\d+.\\d+', edge_string) and not re.fullmatch('\\d+,\\d+,\\d+', edge_string):
+                return False
+    return True
 
 
 algorithms = {
@@ -99,7 +122,7 @@ algorithms = {
 
 
 def calc_response(type, edges, top_nodes):
-    if type =='non_weighted':
+    if type == 'non_weighted':
         logging.log(level=logging.DEBUG, msg=f"calc_response: non_weighted")
 
         G = nx.Graph(edges)
@@ -108,7 +131,6 @@ def calc_response(type, edges, top_nodes):
 
         G = nx.Graph()
         G.add_weighted_edges_from(edges)
-
 
     current_algo = algorithms[type]
 
